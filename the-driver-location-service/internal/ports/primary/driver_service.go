11796425872e19cb29244c6e6a8ca0e1ade158bd
enespolat24 <enespolat24@gmail.com>
2025-07-23 02:47:20 +0300

    feat: implement DriverService and DriverRepository interfaces for driver operations

--- the-driver-location-service/internal/ports/primary/driver_service.go
+++ the-driver-location-service/internal/ports/primary/driver_service.go
@@ -1,1 +1,12 @@
 package primary
+
+import "the-driver-location-service/internal/domain"
+
+type DriverService interface {
+	CreateDriver(req domain.CreateDriverRequest) (*domain.Driver, error)
+	BatchCreateDrivers(req domain.BatchCreateRequest) ([]*domain.Driver, error)
+	SearchNearbyDrivers(req domain.SearchRequest) ([]*domain.DriverWithDistance, error)
+	GetDriver(id string) (*domain.Driver, error)
+	UpdateDriverLocation(id string, location domain.Point) error
+	DeleteDriver(id string) error
+}
--- the-driver-location-service/internal/ports/secondary/driver_repository.go
+++ the-driver-location-service/internal/ports/secondary/driver_repository.go
@@ -1,1 +1,13 @@
 package secondary
+
+import "the-driver-location-service/internal/domain"
+
+// this is  the interface for driver data operations (Secondary Port)
+type DriverRepository interface {
+	Create(driver *domain.Driver) error
+	BatchCreate(drivers []*domain.Driver) error
+	SearchNearby(location domain.Point, radiusMeters float64, limit int) ([]*domain.DriverWithDistance, error)
+	GetByID(id string) (*domain.Driver, error)
+	Update(driver *domain.Driver) error
+	Delete(id string) error
+}
