65ce76517316e9363e8808554576466f153a420c
enespolat24 <enespolat24@gmail.com>
2025-07-23 03:06:14 +0300

    feat: implement Haversine distance calculation for Point struct

--- the-driver-location-service/internal/domain/driver.go
+++ the-driver-location-service/internal/domain/driver.go
@@ -1,6 +1,7 @@
 package domain
 
 import (
+	"math"
 	"time"
 )
 
@@ -51,5 +52,26 @@ func (p Point) Latitude() float64 {
 }
 
 func (p Point) Distance(other Point) float64 {
-	return 00.0 // TODO: Implement distance calculation logic by using Haversine formula
+	return HaversineDistance(p.Latitude(), p.Longitude(), other.Latitude(), other.Longitude())
+}
+
+func HaversineDistance(lat1, lon1, lat2, lon2 float64) float64 {
+	const R = 6371000 // Earth radius in meters
+
+	// Convert degrees to radians
+	lat1Rad := lat1 * math.Pi / 180
+	lon1Rad := lon1 * math.Pi / 180
+	lat2Rad := lat2 * math.Pi / 180
+	lon2Rad := lon2 * math.Pi / 180
+
+	dLat := lat2Rad - lat1Rad
+	dLon := lon2Rad - lon1Rad
+
+	// this is where the fun begins
+	a := math.Sin(dLat/2)*math.Sin(dLat/2) +
+		math.Cos(lat1Rad)*math.Cos(lat2Rad)*
+			math.Sin(dLon/2)*math.Sin(dLon/2)
+	c := 2 * math.Atan2(math.Sqrt(a), math.Sqrt(1-a))
+
+	return R * c
 }
